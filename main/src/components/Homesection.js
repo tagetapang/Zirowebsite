790311429ad012b5632ab9ee6ef399311a3a6793
tage tapang <tagetage70@gmail.com>
2022-12-30 13:30:57 +0530

    Revert "messed up"
    This reverts commit 6f2320515eb6f7ef25c18e558caadad4321f605a.

--- main/src/components/Homesection.js
+++ main/src/components/Homesection.js
@@ -1,19 +1,14 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
-import "./Homesection.css";
 import React, { Component } from "react";
 
 export class Homesection extends Component {
   render() {
     return (
-      <div className="container">
-        <div class="container horizontal-scrollable">
-                <div class="row text-center">
-                    
-                </div>
-        </div>
-      
-            _______________ */}{/* _______________________________________________________________________________________ */}
-            <div class="card m-auto" style={{ width: "18rem",height: '15rem' }}>
+      <div className="row ">
+        <div className="col-3 bg-dark">
+          <div class="overflow-auto" style={{height: '100vh',width: '20rem'}}>
+            {/* _______________________________________________________________________________________ */}
+            <div class="card m-auto" style={{ width: "18rem" }}>
               <img src="..." class="card-img-top" alt="..." />
               <div class="card-body">
                 <h5 class="card-title">Card title</h5>
@@ -28,7 +23,7 @@ export class Homesection extends Component {
             </div>
             {/* _______________________________________________________________________________________ */}
             {/* _______________________________________________________________________________________ */}
-            <div class="card m-auto" style={{ width: "18rem",height: '15rem' }}>
+            <div class="card m-auto" style={{ width: "18rem" }}>
               <img src="..." class="card-img-top" alt="..." />
               <div class="card-body">
                 <h5 class="card-title">Card title</h5>
@@ -43,7 +38,7 @@ export class Homesection extends Component {
             </div>
             {/* _______________________________________________________________________________________ */}
             {/* _______________________________________________________________________________________ */}
-            <div class="card m-auto" style={{ width: "18rem",height: '15rem' }}>
+            <div class="card m-auto" style={{ width: "18rem" }}>
               <img src="..." class="card-img-top" alt="..." />
               <div class="card-body">
                 <h5 class="card-title">Card title</h5>
@@ -58,7 +53,7 @@ export class Homesection extends Component {
             </div>
             {/* _______________________________________________________________________________________ */}
             {/* _______________________________________________________________________________________ */}
-            <div class="card m-auto" style={{ width: "18rem",height: '15rem' }}>
+            <div class="card m-auto" style={{ width: "18rem" }}>
               <img src="..." class="card-img-top" alt="..." />
               <div class="card-body">
                 <h5 class="card-title">Card title</h5>
@@ -72,54 +67,9 @@ export class Homesection extends Component {
               </div>
             </div>
             {/* _______________________________________________________________________________________ */}
-            {/* _______________________________________________________________________________________ */}
-            <div class="card m-auto" style={{ width: "18rem",height: '15rem' }}>
-              <img src="..." class="card-img-top" alt="..." />
-              <div class="card-body">
-                <h5 class="card-title">Card title</h5>
-                <p class="card-text">
-                  Some quick example text to build on the card title and make up
-                  the bulk of the card's content.
-                </p>
-                <a href="#" class="btn btn-primary">
-                  Go somewhere
-                </a>
-              </div>
-            </div>
-            {/* _______________________________________________________________________________________ */}
-            {/* _______________________________________________________________________________________ */}
-            <div class="card m-auto" style={{ width: "18rem",height: '15rem' }}>
-              <img src="..." class="card-img-top" alt="..." />
-              <div class="card-body">
-                <h5 class="card-title">Card title</h5>
-                <p class="card-text">
-                  Some quick example text to build on the card title and make up
-                  the bulk of the card's content.
-                </p>
-                <a href="#" class="btn btn-primary">
-                  Go somewhere
-                </a>
-              </div>
-            
-            {/* _______________________________________________________________________________________ */}
-            {/* _______________________________________________________________________________________ */}
-            <div class="card m-auto" style={{ width: "18rem",height: '15rem' }}>
-              <img src="..." class="card-img-top" alt="..." />
-              <div class="card-body">
-                <h5 class="card-title">Card title</h5>
-                <p class="card-text">
-                  Some quick example text to build on the card title and make up
-                  the bulk of the card's content.
-                </p>
-                <a href="#" class="btn btn-primary">
-                  Go somewhere
-                </a>
-              </div>
-            </div>
-            
           </div>
-        
-        
+        </div>
+        <div className="col">this is the col test</div>
       </div>
     );
   }
