42b67d6049c0519edbee07901f5b546314215e44
tage tapang <tagetage70@gmail.com>
2022-12-30 21:51:16 +0530

    failed
    i think my image is not that clear that is why im having so much difficulty

--- main/src/App.js
+++ main/src/App.js
@@ -1,5 +1,6 @@
 import "./App.css";
 import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
 import Homesection from "./components/Homesection";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
@@ -10,11 +11,11 @@ function App() {
       <Navbar />
       <BrowserRouter>
         <Routes>
-            <Route path="/" element={<Homesection />}>
-
-          </Route>
+            <Route path="/" element={<Homesection />}/>
+          
         </Routes>
       </BrowserRouter>
+      <Footer></Footer>
     </>
   );
 }
---
+++ main/src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Footer() {
+  return (
+    <div className='d-flex  bg-black' style={{height: '30px',flexDirection: 'row'}}>
+        <p>this is the footer</p>
+        
+      
+    </div>
+  )
+}
