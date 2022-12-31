26ec16089be332f26ac75774592b9ec4a802e70d
tage tapang <tagetage70@gmail.com>
2022-12-31 13:41:38 +0530

    made the home and navbar
    this time im statisfyed with the result and wont be twiking the nav bar and home horizontal scroll components that ive created in this commit

--- main/src/components/Navbar.js
+++ main/src/components/Navbar.js
@@ -1,47 +1,23 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import React from "react";
+import "./nav.css";
 
 export default function Navbar() {
   return (
-    <div>
-      <nav className="navbar navbar-expand-lg bg-dark">
-        <div className="container-fluid">
-          <a className="navbar-brand mx-3" href="#"  style={{color: 'white'}}>
-            AllZiro
-          </a>
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarNav"
-            aria-controls="navbarNav"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse" id="navbarNav">
-            <ul className="navbar-nav">
-              <li className="nav-item">
-                <a className="nav-link active" aria-current="page" href="#" style={{color: 'white'}}>
-                  Home
-                </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#"  style={{color: 'white'}}>
-                  about us
-                </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#"  style={{color: 'white'}}>
-                  contact us
-                </a>
-              </li>
-             
-            </ul>
-          </div>
-        </div>
-      </nav>
-    </div>
+    <>
+      <nav className="navbar sticky-top">
+        
+      
+
+        <ul className="navcontainer">
+          <div className="title"><h3><a href="#">AllZiro </a></h3></div>
+  
+          <li><a href="#">Home</a></li>
+          <li><a href="#">about</a></li>
+          <li><a href="#">contact us</a></li>
+        </ul>
+      </nav>  
+
+    </>
   );
 }
