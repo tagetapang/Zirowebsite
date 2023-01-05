83c3872269a8f0bc4c08136201f11c5281880ad9
tage tapang <tagetage70@gmail.com>
2023-01-05 12:46:10 +0530

    made hija and mudang tage section
    made what said in the commit and have more to do

--- main/src/components/aboutvillages/Hija.js
+++ main/src/components/aboutvillages/Hija.js
@@ -1,16 +1,24 @@
 import React from 'react'
+import './vill.css'
 
 export default function Hija() {
   return (
     <>
-    <div className="biggest">
-        <div className="contain">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit dolores maiores corrupti voluptatibus eos blanditiis non repellat fuga earum odio iure reiciendis rerum, similique omnis aperiam error iste incidunt vel in illo suscipit! Veniam.
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus autem accusantium quam repellat animi. Impedit excepturi consequatur amet labore repudiandae error obcaecati, dolor nesciunt! Repellendus iste enim quidem dolorem blanditiis placeat suscipit exercitationem! Eum accusantium mollitia nesciunt animi, ratione corrupti blanditiis quam nam reiciendis tempore nostrum architecto maxime numquam minima sit amet, dicta ea accusamus fugiat. Voluptatibus minus ipsam deserunt. Impedit voluptatem asperiores eveniet, rem molestiae in necessitatibus voluptas tempora perferendis aliquid a quod pariatur ut itaque repellendus minima inventore ab quasi minus provident, quaerat maiores ea voluptatum? Laboriosam magnam deserunt ab ex dolore perspiciatis consequatur pariatur, omnis voluptate inventore.
-        </div>
-        <div className="contain2">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam culpa eum quibusdam quisquam doloribus voluptatum autem sequi placeat repellat nobis dolorem suscipit, commodi ea molestias et adipisci, fugiat dolore. Officia eum voluptates amet ipsam!
-        </div>
+     <div className="biggest bg-dark">
+      <h1>hija village</h1>
+
+      <div className="contain">
+        <p>hija is a very boring place with lots of tugs in its area Lorem, ipsum dolor sit amet consectetur adipisicing elit. Animi nihil aspernatur natus nisi ipsa exercitationem praesentium consectetur dolorem, ex odit minima impedit illum.</p>
+        <ul>
+        <li>pura</li>
+        <li>ada</li>
+        <li>tora</li>
+        <li>sasta</li>
+        </ul>
+        <p>it is a not so beautiful place with lots of agricultural land near its boundries</p>
+      </div>
+      
+       
     </div>
     </>
   )
--- main/src/components/aboutvillages/Mudangtage.js
+++ main/src/components/aboutvillages/Mudangtage.js
@@ -4,13 +4,24 @@ import './vill.css'
 export default function Mudangtage() {
   return (
     <>
-    <div className="biggest">
+    <div className="biggest bg-dark">
+      <h1>mudang tage village</h1>
+
         <div className="contain">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit dolores maiores corrupti voluptatibus eos blanditiis non repellat fuga earum odio iure reiciendis rerum, similique omnis aperiam error iste incidunt vel in illo suscipit! Veniam.
+           <p>mudang tage is located near the place where music festival is held.
+           its neighbour are bamin michi and dutta village.
+           The surnames that belongs to this village are-</p>
+           <ul>
+            <li>tage</li>
+            <li>mudang</li>
+            <li>tadu</li>
+            <li>buru</li>
+            <li>leggang</li>
+           </ul>
+           <p>it is a beautiful place with lots of agricultural land near its boundries</p>
         </div>
-        <div className="contain2">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam culpa eum quibusdam quisquam doloribus voluptatum autem sequi placeat repellat nobis dolorem suscipit, commodi ea molestias et adipisci, fugiat dolore. Officia eum voluptates amet ipsam!
-        </div>
+       
+       
     </div>
     </>
   )
